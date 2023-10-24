286b9ff59da8cd8e30d74bb4fc1d5bfd401aa92a
haneen-daoud <haneen2592002@gmail.com>
2023-10-24 19:49:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Foter from "./Foter";
+
+
+export default function App(){
+return(
+<>
+<Navbar/>
+
+<Home/>
+<Foter/>
+</>
+);
+}
